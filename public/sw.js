f89e9b4ca699eb626fef4a53f300ab1993c9393a
jack <juseog0373@naver.com>
2026-08-26 23:05:04 +0900

    Refresh service worker after editor update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mygrid-shell-v3";
+const CACHE_NAME = "mygrid-shell-v4";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(
