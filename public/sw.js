a995561eb460086a5800fb67f1b962f2ff542e9a
jack <juseog0373@naver.com>
2026-08-26 23:16:34 +0900

    Refresh shell for gesture editor

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mygrid-shell-v4";
+const CACHE_NAME = "mygrid-shell-v5";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(
